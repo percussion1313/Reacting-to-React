51efec8fcecc647c650f1b9b3f4444d5a825345f
percussion1313 <peterjacobgrant@gmail.com>
2018-06-14 13:41:31 -0500

    First commit

---
+++ src/components/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = (props) => {
+    return <h1>{props.name}</h1>;
+   };
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app.jsx'
+
+
+ReactDOM.render(<App name="I don't think this is right at all" />,
+document.getElementById('root'));
