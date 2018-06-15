d9963e4adeb8048556949546ccd5ef9aadf3cbc5
percussion1313 <peterjacobgrant@gmail.com>
2018-06-15 10:30:22 -0500

    Added button with functionality

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,27 +10,45 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: 'Inigo Montoya'
+            name: 'Inigo Montoya',
+            hasLoaded: false
         }
         this.handleNameChange = this.handleNameChange.bind(this);
-
+        this.hasLoaded = this.hasLoaded.bind(this);
         
     }
 
+    hasLoaded() {
+        this.setState({hasLoaded: !this.state.hasLoaded});
+    }
+
+    componentDidMount() {
+
+    }
+    
     handleNameChange(event) {
         // do something useful
         this.setState({name: event.target.value})
         console.log('Typey Typey');
-
       }
 
     render() {
-        return (
-            <React.Fragment>
-                <h1>My name is {this.state.text}, you killed my {this.props.relative.name}, prepare to die.</h1>
-                <input type="text" placeholder={this.state.name} value={this.state.name} onChange={this.handleNameChange}></input>
-            </React.Fragment>
-        )
+        if (this.state.hasLoaded) {
+            return (
+                <React.Fragment>
+                    <h1>My name is {this.state.name}, you killed my {this.props.relative.name}, prepare to die.</h1>
+                    <input type="text" placeholder={this.state.name} value={this.state.name} onChange={this.handleNameChange}></input>
+                    <button onClick={this.hasLoaded}>Click Me</button>            
+                    </React.Fragment>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <h1>Loading...</h1>
+                    <button onClick={this.hasLoaded}>Click Me</button>
+                </React.Fragment>
+            )
+        }
     }
 }
 
